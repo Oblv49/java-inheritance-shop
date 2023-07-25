d771e6eca5632b698a38490698d75069ff4ae990
DavideMinutoli <noprivacyworld@gmail.com>
2023-07-25 18:32:48 +0200

    add methods for rundom num for biginteger imei

--- JavaShopExercise/src/org/java/lessons/shop/Smartphone.java
+++ JavaShopExercise/src/org/java/lessons/shop/Smartphone.java
@@ -1,5 +1,8 @@
 package org.java.lessons.shop;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 public class Smartphone extends Product {
 
     //variables declaration
@@ -27,4 +30,27 @@ public class Smartphone extends Product {
         this.codIMEI = codIMEI;
         this.memory = memory;
     }
+
+    //private methods
+    private String codIMEIGenerator() {
+            //max lim variables
+        BigInteger maxLimit = new BigInteger("999999999999999");
+            //random variables
+        Random random = new Random();
+            //creation numRandom
+        BigInteger randomNum = new BigInteger(maxLimit.bitLength(), random);
+        while (randomNum.compareTo(maxLimit) > 0) {
+            randomNum = new BigInteger(maxLimit.bitLength(), random);
+        }
+        // conversion to string and add 0 to sx
+        String codIMEI = randomNum.toString();
+        while (codIMEI.length() < 15) {
+            codIMEI = "0" + codIMEI;
+        }
+        return codIMEI;
+    }
+
+
+
 }
+
